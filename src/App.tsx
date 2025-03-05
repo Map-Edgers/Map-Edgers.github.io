d85df81e57c6863ffee52efdd4f352d7a2bbf1fb
FormalStatic <karenassassin@gmail.com>
2025-03-04 20:12:23 -0700

    TEst

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import './styles.css'
 
 function App() {
-  const webhookURL = import.meta.env.WEBHOOK;
+  const webhookURL = import.meta.env.VITE_WEBHOOK;
 
   function discord_message(message: any) {
     var xhr = new XMLHttpRequest();
